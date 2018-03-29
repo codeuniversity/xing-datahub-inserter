7749a0b00016fe8b085292c320741ca4504109f0
alexmorten <alexandrius.martinus@gmx.de>
2018-03-29 19:49:53 +0200

    (refactor) use docker for running test

--- integration.sh
+++ integration.sh
@@ -2,15 +2,19 @@
 
 docker-compose -f docker-compose.test.yml up -d
 sleep 160
+docker build -t inserter_test .
+docker run -p 3002:3002 --net="host" -d --rm --name tested inserter_test
 
 python inserter_test.py
 if [ $? -eq 1 ]
 then
   echo 'test failed' >&2
   docker-compose -f docker-compose.test.yml down
+  docker stop tested
   exit 1
 else
   echo 'test was successful'
   docker-compose -f docker-compose.test.yml down
+  docker stop tested
   exit 0
 fi
